b20abef6bac14d799c5ee256c30a06a9fb74258d
Varga Tímea <vargatimi505@mailbox.unideb.hu>
2025-04-15 09:35:47 +0200

    Database seeding

--- src/main/java/com/esen/bookstore/model/Bookstore.java
+++ src/main/java/com/esen/bookstore/model/Bookstore.java
@@ -1,9 +1,8 @@
 package com.esen.bookstore.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import java.util.Map;
 
-@Data
 @Entity
 public class Bookstore {
 
@@ -15,4 +14,70 @@ public class Bookstore {
     private String location;
     private Double priceModifier;
     private Double moneyInCashRegister;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Map<Book, Integer> inventory;
+
+    public Bookstore() {
+    }
+
+    public Bookstore(Long id, String location, Double priceModifier, Double moneyInCashRegister, Map<Book, Integer> inventory) {
+        this.id = id;
+        this.location = location;
+        this.priceModifier = priceModifier;
+        this.moneyInCashRegister = moneyInCashRegister;
+        this.inventory = inventory;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getPriceModifier() {
+        return priceModifier;
+    }
+
+    public void setPriceModifier(Double priceModifier) {
+        this.priceModifier = priceModifier;
+    }
+
+    public Double getMoneyInCashRegister() {
+        return moneyInCashRegister;
+    }
+
+    public void setMoneyInCashRegister(Double moneyInCashRegister) {
+        this.moneyInCashRegister = moneyInCashRegister;
+    }
+
+    public Map<Book, Integer> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Map<Book, Integer> inventory) {
+        this.inventory = inventory;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Bookstore{");
+        sb.append("id=").append(id);
+        sb.append(", location='").append(location).append('\'');
+        sb.append(", priceModifier=").append(priceModifier);
+        sb.append(", moneyInCashRegister=").append(moneyInCashRegister);
+        sb.append(", inventory=").append(inventory);
+        sb.append('}');
+        return sb.toString();
+    }
 }
